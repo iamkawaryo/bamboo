7662a47330ffb95ce2cecbc2df130762f76cdedf
iamkawaryo <ryosukekawahara0725@gmail.com>
2024-07-05 15:56:37 +0900

    Add files via upload
    試作の計算ゲームです

---
+++ gamebox/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import random, time
+from mathgame import mathgame_logic
+
+app = Flask(__name__)
+app.secret_key = 'bamboo'  # セッション用の秘密鍵を設定
+
+@app.route('/')
+def start():
+    return render_template("home.html")
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/mathgame", methods=["GET"])
+def mathgame():
+    start_time = time.time()  # タイマースタート
+    num1 = random.randint(1, 30)
+    num2 = random.randint(1, 30)
+    session['start_time'] = start_time #セッションに開始時間を保存
+    session['num1'] = num1  # セッションにnum1を保存
+    session['num2'] = num2  # セッションにnum2を保存
+    session['count'] = 0  # 初期のカウントをセッションに保存
+    return render_template("mathgame.html", num1=num1, num2=num2)
+
+@app.route("/start_mathgame", methods=["GET"])
+def start_mathgame():
+    num1 = session.get('num1', 0)
+    num2 = session.get('num2', 0)
+    user_ans = int(request.args.get('user_ans', 0))  # ユーザー入力を取得
+    count = session.get('count', 0)  # セッションからカウントを取得
+
+    rendered_template = mathgame_logic(num1, num2, user_ans, count)
+    
+    # 数値を取り出してセッションを更新
+    if "Correct! Try the next one." in rendered_template:
+        session['num1'] = random.randint(1, 30)
+        session['num2'] = random.randint(1, 30)
+        session['count'] = count + 1
+        return redirect(url_for('start_mathgame'))
+    elif "Incorrect, try again!" in rendered_template:
+        return rendered_template
+    else:
+        return rendered_template
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888)
---
+++ gamebox/mathgame.py
@@ -0,0 +1,21 @@
+import random,time
+from flask import render_template, session
+
+def mathgame_logic(num1, num2, user_ans, count):
+    ans = num1 + num2
+
+    if ans == user_ans:
+        count += 1
+        if count < 10:
+            num1 = random.randint(1, 30)
+            num2 = random.randint(1, 30)
+            print(count)
+            return render_template("mathgame.html", num1=num1, num2=num2, count=count, message="Correct! Try the next one.")
+        else:
+            end_time = time.time()  # タイマーストップ
+
+            start_time = session.get('start_time',0)
+            score = end_time - start_time  # スコアの計算
+            return render_template("mathgame_result.html", score = score)  # 結果を表示するテンプレート
+    else:
+        return render_template("mathgame.html", num1=num1, num2=num2, count=count, message="Incorrect, try again!")
